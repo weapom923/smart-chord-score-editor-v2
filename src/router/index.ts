0371682b3ad9b26dfba29f8fdbd4c70649cba7cd
weapom923 <kenshiro.ueda@gmail.com>
2023-07-23 14:50:31 +0900

    軽微なリファクタ

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,34 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import App from '../App.vue';
 import { Score } from '../modules/Score';
 
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/:pathMatch(.*)*',
-    component: App,
-    async beforeEnter(to, from, next) {
-      if (['/', ''].includes(to.path)) {
-        let scoreJsonFromCookie = window.localStorage.getItem('score');
-        if (scoreJsonFromCookie !== null) {
-          to.meta.score = Score.loadJson(scoreJsonFromCookie);
-        } 
-        next();
-      } else {
-        try {
-          let response = await fetch(`/scores${to.path}.json`);
-          let scoreRawObj = await response.json();
-          to.meta.score = Score.loadFromRawObj(scoreRawObj);
-        }
-        finally {
-          next();
-        }
-      }
-    },
-    props: route => ({ score: route.meta.score }),
-  },
-]
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes: [
+    {
+      path: '/:pathMatch(.*)*',
+      component: App,
+      async beforeEnter(to, from, next) {
+        if (['/', ''].includes(to.path)) {
+          let scoreJsonFromCookie = window.localStorage.getItem('score');
+          if (scoreJsonFromCookie !== null) {
+            to.meta.score = Score.loadJson(scoreJsonFromCookie);
+          } 
+          next();
+        } else {
+          try {
+            let response = await fetch(`/scores${to.path}.json`);
+            let scoreRawObj = await response.json();
+            to.meta.score = Score.loadFromRawObj(scoreRawObj);
+          }
+          finally {
+            next();
+          }
+        }
+      },
+      props: route => ({ score: route.meta.score }),
+    },
+  ],
 })
 
 export default router
