5d88f0e41405e7948fa7b4f7b63e728814f1d76d
weapom923 <kenshiro.ueda@gmail.com>
2023-07-23 14:50:12 +0900

    冗長なスコアデータ作成処理を除外

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,28 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import App from '../App.vue';
 import { Score } from '../modules/Score';
-import { ScoreMetadata } from '../modules/ScoreMetadata';
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '',
+    path: '/:pathMatch(.*)*',
     component: App,
     async beforeEnter(to, from, next) {
       if (['/', ''].includes(to.path)) {
         let scoreJsonFromCookie = window.localStorage.getItem('score');
-        if (scoreJsonFromCookie === null) {
-          to.meta.score = new Score();
-        } else {
+        if (scoreJsonFromCookie !== null) {
           to.meta.score = Score.loadJson(scoreJsonFromCookie);
         } 
+        next();
       } else {
         try {
           let response = await fetch(`/scores${to.path}.json`);
           let scoreRawObj = await response.json();
           to.meta.score = Score.loadFromRawObj(scoreRawObj);
-        } catch {
-          to.meta.score = new Score(new ScoreMetadata('Untitled'));
+        }
+        finally {
+          next();
         }
       }
-      next();
     },
     props: route => ({ score: route.meta.score }),
   },
